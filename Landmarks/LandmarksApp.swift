d9f8cd43a5586b07bfb18543062d0e1baa8f8d30
Askhat <baiaskhat@gmail.com>
2026-01-21 20:56:09 +0500

    Initial commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Асхат Баймуканов on 21.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
